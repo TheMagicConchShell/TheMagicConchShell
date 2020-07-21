deba11db44de61e7332476ea33e06252f84f4e87
Kang Myun Hun <lizingmaster@gmail.com>
2020-07-21 17:16:34 +0900

    S03P12A403-71 notice backend

---
+++ backend/src/main/java/com/blsa/ezilog/dao/NoticeDao.java
@@ -0,0 +1,13 @@
+package com.blsa.ezilog.dao;
+
+import java.math.BigInteger;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.blsa.ezilog.model.notice.Notice;
+
+public interface NoticeDao extends JpaRepository<Notice, String> {
+	List<Notice> getNoticeByWriter(String writer) throws Exception;
+	Notice getNoticeByNid(BigInteger id) throws Exception;
+}
