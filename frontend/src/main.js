1f4bda2f6ccce10342c45f3d4af1792d1f9da4b5
김성민 <dia321@naver.com>
2020-07-21 11:06:16 +0900

    S03P12A403-68 bootstrap setting

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue';
 import axios from 'axios';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 const vm = new Vue({
     router,
