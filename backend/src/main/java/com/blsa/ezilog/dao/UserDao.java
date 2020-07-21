abd61098626ed9525a0fd7e23ee2b35d001f99cd
lshmn951 <tjd951@naver.com>
2020-07-21 18:04:39 +0900

    S03P12A403-75 Implement Update user

--- backend/src/main/java/com/blsa/ezilog/dao/UserDao.java
+++ backend/src/main/java/com/blsa/ezilog/dao/UserDao.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.blsa.ezilog.model.user.User;
 
 public interface UserDao extends JpaRepository<User, Long> {
-	User findByUid(long uid);
+	Optional<User> findByUid(long uid);
 	Optional<User> findByEmailAndPassword(String email,String password);
 	
 	Optional<User> findByEmail(String email);
--- backend/src/main/java/com/blsa/ezilog/service/UserService.java
+++ backend/src/main/java/com/blsa/ezilog/service/UserService.java
@@ -2,10 +2,13 @@ package com.blsa.ezilog.service;
 
 import com.blsa.ezilog.model.user.LoginRequestDTO;
 import com.blsa.ezilog.model.user.SignupRequestDTO;
+import com.blsa.ezilog.model.user.UpdateRequestDTO;
 import com.blsa.ezilog.model.user.User;
 
 public interface UserService {
 	User signup(SignupRequestDTO request);
 	User login(LoginRequestDTO request);
+	User update(UpdateRequestDTO request);
+	User select(Long uid);
 	String duplicateCheck(String email,String nickname);
 }
