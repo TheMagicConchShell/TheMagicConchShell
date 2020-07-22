c7de9aaa3391304c77b2b6d445872fe01932ee03
서정하 <seoroda@gmail.com>
2020-07-22 15:11:35 +0900

    S03P12A403-79 add signup func front

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,8 @@ import VueRouter from 'vue-router';
 import Home from '../views/Platform/Home.vue';
 import Blog from '../views/Blog/Blog.vue';
 
+import Signup from '../components/user/Signup.vue';
+
 Vue.use(VueRouter);
 
 const routes = [
@@ -16,6 +18,11 @@ const routes = [
         path: '/blog',
         name: 'Blog',
         component: Blog,
+    },
+    {
+        path: '/user/signup',
+        name: 'signup',
+        component: Signup,
     },
 ];
 
