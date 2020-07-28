c1243bbe28f4a1d16ba07df1d22db8d7638cfc8b
서정하 <seoroda@gmail.com>
2020-07-28 11:23:37 +0900

    S03P12A403-139 add select main counsel func backend

---
+++ backend/src/main/java/com/blsa/ezilog/model/post/SelectMainPostRequestDTO.java
@@ -0,0 +1,26 @@
+package com.blsa.ezilog.model.post;
+
+import java.math.BigInteger;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Valid
+@Getter
+@Setter
+@ToString
+public class SelectMainPostRequestDTO {
+    @ApiModelProperty(required = true)
+    @NotNull
+    BigInteger no;
+    
+    @ApiModelProperty(required = true)
+    @NotBlank(message = "설명은 필수 입력값입니다.")
+    String description;
+}
