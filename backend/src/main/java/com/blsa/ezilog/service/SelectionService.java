13087ad5493d677f3eb5f8066e5af4bb592a7238
서정하 <seoroda@gmail.com>
2020-07-28 14:12:49 +0900

    Implement remove from main counsel
    - 메인에서 글을 내리는 기능 추가 (메인에서 삭제, 히스토리에 추가)

---
+++ backend/src/main/java/com/blsa/ezilog/service/SelectionService.java
@@ -0,0 +1,38 @@
+package com.blsa.ezilog.service;
+
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.blsa.ezilog.dao.SelectionHistoryDao;
+import com.blsa.ezilog.dao.SelectionPostDao;
+import com.blsa.ezilog.model.post.SelectionHistory;
+import com.blsa.ezilog.model.post.SelectionPost;
+
+public class SelectionService{
+
+    @Autowired
+    SelectionPostDao selectionPostDao;
+    
+    @Autowired
+    SelectionHistoryDao selectionHistoryDao;
+    
+    @Transactional
+    public SelectionHistory removeMainPostandAddtoHistory(SelectionPost post) {
+        Optional<SelectionPost> selectionPostOptional = selectionPostDao.findById(post.getId());
+        
+        if(selectionPostOptional.isPresent()) {
+            SelectionHistory selectionHistory = new SelectionHistory(post.getId(), post.getNo(), post.getDescription());
+            selectionHistoryDao.save(selectionHistory);
+            selectionPostDao.deleteById(post.getId());
+            
+            return selectionHistory;
+        }else {
+            return null;
+        }
+        
+    }
+
+}
