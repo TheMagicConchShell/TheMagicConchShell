4211d0ab3901678d1831f24ebf9e36b341ca95d3
Hyeon-uk <tristan3716@gmail.com>
2020-08-03 12:53:26 +0900

    S03P13A403-63 lint-staged pre-commit 적용

--- frontend/src/views/Platform/Home.vue
+++ frontend/src/views/Platform/Home.vue
@@ -45,9 +45,11 @@
                     <p class="d-flex">
                         written by {{ nowshowing.writer }}
                     </p>
+                    <!-- eslint-disable -->
                     <viewer
                         v-html="nowshowing.content"
                     />
+                    <!-- eslint-disable -->
                 </div>
                 <div v-else>
                     이런... 사이트가 망해서 고민이 없습니다ㅠㅠ
