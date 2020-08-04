99d4e22bf53d8bc06afee70b93659e7c95eab80d
lshmn951 <tjd951@naver.com>
2020-08-04 11:10:58 +0900

    S03P13A403-45 스케줄러 서비스 구현

---
+++ backend/src/main/java/com/blsa/ezilog/model/post/RecommendPost.java
@@ -0,0 +1,35 @@
+package com.blsa.ezilog.model.post;
+
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecommendPost {
+    private BigInteger no;
+
+    private String writer;
+
+    private BigInteger categoryId;
+
+    private String title;
+
+    private String content;
+
+    private LocalDateTime writeDate;
+
+    private int likeCount;
+
+    private int unlikeCount;
+
+    private int views;
+
+    private int replyCount;
+
+    private int total;
+}
