250140cc1916d088dab62c04819cee195598137e
서정하 <seoroda@gmail.com>
2020-08-10 13:53:56 +0900

    내 고민 목록 조회시 undefined 오류 수정

--- frontend/src/components/account/MyPost.vue
+++ frontend/src/components/account/MyPost.vue
@@ -15,14 +15,14 @@
                     style="max-height: 200px;"
                 >
                     <div class="d-flex w-100 justify-content-between">
-                        <div>
+                        <div class="item">
                             <span id="title">
                                 {{ item.title }}
                             </span>
                             <span id="divider">|</span>
                             <span>{{ getCategory(item.categoryId) }}</span>
                         </div>
-                        <div>
+                        <div class="item">
                             <span>{{ getFormatDate(item.writeDate) }} </span> 
                             <span id="divider">|</span>
                             <span>{{ item.views }}</span>
@@ -117,6 +117,7 @@ export default {
             });
         },
         getCategory(categoryId){
+            this.$store.dispatch('fetchCategories');
             return this.$store.getters.categoryNameById(categoryId);
         },
         getFormatDate(date) {
@@ -144,7 +145,13 @@ export default {
     color: lightgray;
 }
 #title {
+    max-width: 70%;
     font-size: x-large;
+}
+.item {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap; 
 }
 .content {
     width: 100%;
