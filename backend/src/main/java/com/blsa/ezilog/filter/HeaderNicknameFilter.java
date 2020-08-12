059057a449187cb2adacdfb13df6077fbb7ef73f
Hyeon-uk <tristan3716@gmail.com>
2020-08-12 11:59:30 +0900

    S03P13A403-96 한글 닉네임 디코딩 필터

---
+++ backend/src/main/java/com/blsa/ezilog/filter/HeaderNicknameFilter.java
@@ -0,0 +1,61 @@
+package com.blsa.ezilog.filter;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Enumeration;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+@Component
+public class HeaderNicknameFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(
+            ServletRequest request, 
+            ServletResponse response, 
+            FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequestWrapper wrapper = new HttpServletRequestWrapper((HttpServletRequest) request) {
+            @Override
+            public String getHeader(String name) {
+                if ("nickname".equals(name)) {
+                    try {
+                        return URLDecoder.decode(super.getHeaders(name).nextElement(), "UTF-8");
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return super.getHeader(name);
+            }
+            @Override
+            public Enumeration<String> getHeaders(String name) {
+                if ("nickname".equals(name)) {
+                    try {
+                        Collection<String> result = new ArrayList<String>();
+                        result.add(URLDecoder.decode(super.getHeaders(name).nextElement(), "UTF-8"));
+                       
+                        return Collections.enumeration(result);
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return super.getHeaders(name);
+            }
+        };
+        
+        chain.doFilter(wrapper, response);
+    }
+}
+
