0caf8500045f27e89496bbff5805699ce749eba0
lshmn951 <tjd951@naver.com>
2020-08-17 18:36:59 +0900

    S03P13A403-120 랭킹게시판 UI 변경

--- frontend/src/components/rank/PostTable.vue
+++ frontend/src/components/rank/PostTable.vue
@@ -2,6 +2,15 @@
     <div>
         <template v-if="list && list.length">
             <table class="table">
+                <colgroup>
+                    <col width="8%">
+                    <col width="32%">
+                    <col width="20%">
+                    <col width="10%">
+                    <col width="10%">
+                    <col width="10%">
+                    <col width="10%">
+                </colgroup>
                 <thead class="thead-light">
                     <tr>
                         <th>
@@ -13,25 +22,43 @@
                         <th>
                             작성자
                         </th>
-                        <th>
+                        <th
+                            @click="changeSort(6)"
+                        >
                             추천수
                             <i 
                                 v-if="mySort==6" 
                                 class="fas fa-long-arrow-alt-down"
                             />
+                            <i
+                                v-else
+                                class="fas fa-sort"
+                            />
                         </th>
-                        <th>
+                        <th
+                            @click="changeSort(7)"
+                        >
                             조회수
                             <i 
                                 v-if="mySort==7" 
                                 class="fas fa-long-arrow-alt-down"
                             />
+                            <i
+                                v-else
+                                class="fas fa-sort"
+                            />
                         </th>
-                        <th>
+                        <th
+                            @click="changeSort(8)"
+                        >
                             답변수
                             <i 
                                 v-if="mySort==8" 
                                 class="fas fa-long-arrow-alt-down"
+                            />
+                            <i
+                                v-else
+                                class="fas fa-sort"
                             />
                         </th>
                         <th>
@@ -66,52 +93,61 @@
 
 <script>
 export default {
-    props:{
-        mySort:{
-            type:Number,
-            required:true
-        }
-    },
     data() {
         return {
-            list:[]
+            list:[],
+            mySort:6
         };
     },
     created() {
-        this.$axios({
-            url:"/rank/post",
-            method:"GET",
-            params:{
-                sort:this.mySort
-            }
-        }).then((res)=>{
-            if(res.data.status==="S-200"){
-                let formatDate = function (date) {
-                    let d = new Date(date),
-                        month = '' + (d.getMonth() + 1),
-                        day = '' + d.getDate(),
-                        year = d.getFullYear();
-                    if (month.length < 2) 
-                        month = '0' + month;
-                    if (day.length < 2) 
-                        day = '0' + day;
-                    return [month, day].join('-');
-                };
-                this.list = res.data.data.map((e)=>{
-                    e.writeDate = formatDate(e.writeDate);
-                    e.category = this.$store.getters.categoryNameById(e.categoryId);
-                    return e;
-                });
-            }
-        }).catch(e=>{
-            console.log(e);
-        });
+        this.getList(6);
     },
     methods: {
+        getList(mySort){
+            this.$axios({
+                url:"/rank/post",
+                method:"GET",
+                params:{
+                    sort:mySort
+                }
+            }).then((res)=>{
+                if(res.data.status==="S-200"){
+                    let formatDate = function (date) {
+                        let d = new Date(date),
+                            month = '' + (d.getMonth() + 1),
+                            day = '' + d.getDate(),
+                            year = d.getFullYear();
+                        if (month.length < 2) 
+                            month = '0' + month;
+                        if (day.length < 2) 
+                            day = '0' + day;
+                        return [month, day].join('-');
+                    };
+                    this.list = res.data.data.map((e)=>{
+                        e.writeDate = formatDate(e.writeDate);
+                        e.category = this.$store.getters.categoryNameById(e.categoryId);
+                        return e;
+                    });
+                }
+            }).catch(e=>{
+                console.log(e);
+            });
+        },
+        changeSort(val){
+            this.mySort = val;
+            this.getList(val);
+        },
     },
 };
 </script>
 
 <style>
-
+.table{
+    table-layout: fixed;
+}
+.table td{
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+}
 </style>
--- frontend/src/components/rank/UserTable.vue
+++ frontend/src/components/rank/UserTable.vue
@@ -2,6 +2,14 @@
     <div>
         <template v-if="list && list.length">
             <table class="table">
+                <colgroup>
+                    <col width="10%">
+                    <col width="30%">
+                    <col width="15%">
+                    <col width="15%">
+                    <col width="15%">
+                    <col width="15%">
+                </colgroup>
                 <thead class="thead-light">
                     <tr>
                         <th>
@@ -10,32 +18,56 @@
                         <th>
                             닉네임
                         </th>
-                        <th>
+                        <th
+                            @click="changeSort(2)"
+                        >
                             고민
                             <i 
                                 v-if="mySort==2" 
                                 class="fas fa-long-arrow-alt-down"
                             />
+                            <i
+                                v-else
+                                class="fas fa-sort"
+                            />
                         </th>
-                        <th>
+                        <th
+                            @click="changeSort(3)"
+                        >
                             답변
                             <i 
                                 v-if="mySort==3" 
                                 class="fas fa-long-arrow-alt-down"
                             />
+                            <i
+                                v-else
+                                class="fas fa-sort"
+                            />
                         </th>
-                        <th>
+                        <th
+                            @click="changeSort(4)"
+                        >
                             포인트
                             <i 
                                 v-if="mySort==4" 
                                 class="fas fa-long-arrow-alt-down"
                             />
+                            <i
+                                v-else
+                                class="fas fa-sort"
+                            />
                         </th>
-                        <th>
+                        <th
+                            @click="changeSort(5)"
+                        >
                             레벨
                             <i 
                                 v-if="mySort==5" 
                                 class="fas fa-long-arrow-alt-down"
+                            />
+                            <i
+                                v-else
+                                class="fas fa-sort"
                             />
                         </th>
                     </tr>
@@ -60,15 +92,10 @@
 
 <script>
 export default {
-    props:{
-        mySort:{
-            type:Number,
-            required:true
-        }
-    },
     data() {
         return {
-            list:[]
+            list:[],
+            mySort:2
         };
     },
     created() {
@@ -76,7 +103,7 @@ export default {
             url:"/rank/user",
             method:"GET",
             params:{
-                sort:this.mySort
+                sort:2
             }
         }).then((res)=>{
             if(res.data.status==="S-200"){
@@ -86,7 +113,25 @@ export default {
             console.log(e);
         });
     },
-
+    methods: {
+        changeSort(val){
+            this.mySort = val;
+            this.$axios({
+                url:"/rank/user",
+                method:"GET",
+                params:{
+                    sort:val
+                }
+            }).then((res)=>{
+                if(res.data.status==="S-200"){
+                    this.list = res.data.data;
+                }
+            }).catch(e=>{
+                console.log(e);
+            });
+        },
+        
+    },
 };
 </script>
 
