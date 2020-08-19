fbc89ae6d7583dc5f45d5bfc6ac702343a1a8f8a
lshmn951 <tjd951@naver.com>
2020-08-19 11:11:53 +0900

    S03P13A403-158 채팅 참여 인원 표시

--- backend/src/main/java/com/blsa/ezilog/controller/ChatController.java
+++ backend/src/main/java/com/blsa/ezilog/controller/ChatController.java
@@ -1,34 +1,43 @@
 package com.blsa.ezilog.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-import com.blsa.ezilog.model.chat.ChatMessage;
+import com.blsa.ezilog.model.chat.RequestMessage;
+import com.blsa.ezilog.model.chat.ResponseMessage;
 
 @Controller
 public class ChatController {
+    
+    private int userCount;
+    public ChatController() {
+        userCount = 0;
+    }
+    
     @MessageMapping("/sendMessage")
     @SendTo("/topic/public")
-    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
-        return chatMessage;
+    public ResponseMessage sendMessage(@Payload RequestMessage chatMessage) {
+        ResponseMessage res = new ResponseMessage(chatMessage.getType(), chatMessage.getContent(), chatMessage.getSender(),userCount);
+        return res;
     }
 
     @MessageMapping("/addUser")
     @SendTo("/topic/public")
-    public ChatMessage addUser(@Payload ChatMessage chatMessage,@RequestHeader(value = "nickname", required = true) String nickname){
-        return chatMessage;
+    public ResponseMessage addUser(@Payload RequestMessage chatMessage,@RequestHeader(value = "nickname", required = true) String nickname){
+        userCount++;
+        ResponseMessage res = new ResponseMessage(chatMessage.getType(), chatMessage.getContent(), chatMessage.getSender(), userCount);
+        return res;
     }
     
     @MessageMapping("/leaveUser")
     @SendTo("/topic/public")
-    public ChatMessage leaveUser(@Payload ChatMessage chatMessage) {
-        
-        return chatMessage;
+    public ResponseMessage leaveUser(@Payload RequestMessage chatMessage) {
+        userCount--;
+        ResponseMessage res = new ResponseMessage(chatMessage.getType(), chatMessage.getContent(), chatMessage.getSender(), userCount);
+        return res;
     }
 }
