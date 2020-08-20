72d76acfa5fe4d4d3095c0d62183e1dcbe0096b3
lshmn951 <tjd951@naver.com>
2020-08-20 15:51:20 +0900

    S03P13A403-168 회원 자신의 랭킹 순위 조회 가능

--- backend/src/main/java/com/blsa/ezilog/dao/rank/UserRankDao.java
+++ backend/src/main/java/com/blsa/ezilog/dao/rank/UserRankDao.java
@@ -11,12 +11,12 @@ import com.blsa.ezilog.model.rank.UserRank;
 import com.blsa.ezilog.model.reply.Reply;
 
 public interface UserRankDao extends JpaRepository<UserRank, String> {
-    @Query(value = "select t1.nickname as nickname, count(p.writer) as post_count, t1.rc as reply_count, t1.point as point,t1.level as level,t1.uid as uid\r\n" +
-            "from (select u.uid, count(r.writer) as rc, nickname,point,level from user u left join reply r on u.nickname = r.writer group by u.uid) t1\r\n" +
-            "left join post p on t1.nickname = p.writer\r\n" +
-            "group by t1.nickname\r\n" +
-            "order by ?1 desc, 6 desc\r\n" +
-            "limit 10;", nativeQuery = true)
+    @Query(value = "select A.*,@rownum \\:= @rownum+1 as rnum\r\n" + 
+            "from (select t1.nickname as nickname, count(p.writer) as post_count, t1.rc as reply_count, t1.point as point,t1.level as level,t1.uid as uid\r\n" + 
+            "from (select u.uid, count(r.writer) as rc, nickname,point,level from user u left join reply r on u.nickname = r.writer where u.nickname!='admin' group by u.uid) t1\r\n" + 
+            "left join post p on t1.nickname = p.writer, (select @rownum\\:=0) TMP\r\n" + 
+            "group by t1.nickname\r\n" + 
+            "order by ?1 desc, 6 desc) as A;", nativeQuery = true)
     List<UserRank> selectUserRankData(Integer sort);
     
 }
