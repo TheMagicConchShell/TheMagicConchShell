8c3068cddf5055c9637ca70f1578178eabbb3682
Kang Myun Hun <lizingmaster@gmail.com>
2020-08-20 15:11:14 +0900

    S03P13A403-167 고민 목록 사용자 이미지 나오게 설정
    - 익명의 작성자가 아닐 때 본인 프로필 사진이 나올 수 있게 설정

--- backend/src/main/java/com/blsa/ezilog/controller/CounselController.java
+++ backend/src/main/java/com/blsa/ezilog/controller/CounselController.java
@@ -114,6 +114,8 @@ public class CounselController {
                     }
                     if (e.isSecret() == true) {
                         e.setWriter("익명의 작성자");
+                    }else if(e.isSecret() != true) {
+                        e.setProfileImg(userservice.select(e.getWriter()).getProfileImg());
                     }
                     e.setContent("");
                 });
